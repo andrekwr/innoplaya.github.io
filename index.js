df8d0fce928155fc6fea3ac03f25623358ab4083
Andre-Weber <andreew@al.insper.edu.br>
2022-09-14 23:44:50 -0300

    new template

--- index.js
+++ index.js
@@ -2,46 +2,15 @@ const express = require("express");
 const parser = require("body-parser");
 
 const app = express();
-const urlencodedParser = parser.urlencoded({ extended: true })
-
-app.use(urlencodedParser);
-app.use(parser.json());
-
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/views'));
-app.use(function(req, res, next) {
-    // Website you wish to allow to connect.
-    res.setHeader("Access-Control-Allow-Origin", "*");
-  
-    // Request methods you wish to allow.
-    res.setHeader("Access-Control-Allow-Methods", "POST", "GET");
-  
-    // Request headers you wish to allow.
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
-  
-    next();
-  });
-
-    
 
 
-app.get("/", function(req, res, next) {
-    res.render('temp')
-    
-  });
+var oldTemplate = require('./lib/oldTemplate')
+var newTemplate = require('./lib/newTemplate')
+var temp = require('./lib/temp')
 
+app.use(temp)
+app.use(oldTemplate)
+app.use(newTemplate)
 
-  app.get("/construction", function(req, res, next) {
-    res.render('home')
-    
-  });
-
-  
-  
-
-
-  const port = process.env.PORT || 3002;
-
-app.listen(port, function() {
-    console.log("App running");
-  });
+app.listen(3002)
+console.log("running 3002")
---
+++ index_old.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const parser = require("body-parser");
+
+const app = express();
+const urlencodedParser = parser.urlencoded({ extended: true })
+
+app.use(urlencodedParser);
+app.use(parser.json());
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect.
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  
+    // Request methods you wish to allow.
+    res.setHeader("Access-Control-Allow-Methods", "POST", "GET");
+  
+    // Request headers you wish to allow.
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+  
+    next();
+  });
+
+    
+
+
+app.get("/", function(req, res, next) {
+    res.render('temp')
+    
+  });
+
+
+  app.get("/construction", function(req, res, next) {
+    res.render('home')
+    
+  });
+
+  app.get("/new", function(req, res, next) {
+    console.log()
+    res.render('icoland-package/icolandhtml/home_new')
+    
+  });
+
+  
+  
+
+
+  const port = process.env.PORT || 3002;
+
+app.listen(port, function() {
+    console.log("App running");
+  });
